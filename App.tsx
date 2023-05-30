f97035fa874afa033e8fb28a8e4cb8935bfd063b
Iann Rodrigues <iannrst11@gmail.com>
2023-05-30 13:17:24 -0300

    feat: Cores e fontes da aplicação

--- App.tsx
+++ App.tsx
@@ -1,7 +1,20 @@
+import {
+  Karla_400Regular,
+  Karla_700Bold,
+  useFonts,
+} from '@expo-google-fonts/karla'
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Karla_400Regular,
+    Karla_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
   return (
     <View className="flex-1 items-center justify-center bg-gray-900">
       <Text className="text-gray-200">NativeWind CSS</Text>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./App.tsx', './src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: 'Karla_700Bold',
+        body: 'Karla_400Regular',
+      },
+      colors: {
+        gray: {
+          100: '#F7F7F8',
+          200: '#EDECEE',
+          300: '#D9D8DA',
+          400: '#9F9BA1',
+          500: '#5F5B62',
+          600: '#3E3A40',
+          700: '#1A181B',
+        },
+        red: {
+          400: '#EE7979',
+        },
+        blue: {
+          400: '#647AC7',
+          700: '#364D9D',
+        },
+      },
+    },
   },
   plugins: [],
 }
