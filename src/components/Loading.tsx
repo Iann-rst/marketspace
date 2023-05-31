6c97e1c299f2e6f188b2c25f9cc9237c87a1a394
Iann Rodrigues <iannrst11@gmail.com>
2023-05-31 20:09:20 -0300

    fix: Componente Loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,5 @@
-import { ActivityIndicator, View } from 'react-native'
+import { ActivityIndicator } from 'react-native'
 
 export function Loading() {
-  return (
-    <View className="flex-1 items-center justify-center">
-      <ActivityIndicator className="text-blue-500" />
-    </View>
-  )
+  return <ActivityIndicator className="text-blue-500" />
 }
