4b5918de0deaabf49195bf7f23d0731491f810d3
Iann Rodrigues <iannrst11@gmail.com>
2023-06-02 19:32:49 -0300

    feat: Componente Tag

---
+++ src/components/Tag.tsx
@@ -0,0 +1,28 @@
+import { XCircle } from 'phosphor-react-native'
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+
+type TagProps = TouchableOpacityProps & {
+  isNew?: boolean
+  isActive?: boolean
+}
+
+export function Tag({ isActive = false, isNew = true, ...rest }: TagProps) {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.9}
+      className={`flex-row items-center space-x-1 self-start rounded-full ${
+        isActive ? 'bg-blue-400' : 'bg-gray-300'
+      } px-2 py-0.5`}
+      {...rest}
+    >
+      <Text
+        className={`font-title text-xs uppercase leading-[130%] ${
+          isActive ? 'text-gray-100' : 'text-gray-600'
+        }`}
+      >
+        {isNew ? 'novo' : 'usado'}
+      </Text>
+      {isActive && <XCircle size={16} weight="fill" color="#EDECEE" />}
+    </TouchableOpacity>
+  )
+}
