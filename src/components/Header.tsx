68057913c375b12c21ff821e196c9e56e8c7729c
Iann Rodrigues <iannrst11@gmail.com>
2023-06-02 19:33:00 -0300

    feat: Componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import { ArrowLeft, PencilSimpleLine, Plus } from 'phosphor-react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+
+interface HeaderProps {
+  iconLeft?: boolean
+  title?: string
+  iconRight?: 'plus' | 'edit' | null
+}
+
+export function Header({ iconLeft = true, title, iconRight }: HeaderProps) {
+  return (
+    <View className="w-full flex-row items-center justify-between px-4">
+      {iconLeft ? (
+        <TouchableOpacity activeOpacity={0.6}>
+          <ArrowLeft size={24} color="#1A181B" weight="regular" />
+        </TouchableOpacity>
+      ) : (
+        <View />
+      )}
+
+      {title ? (
+        <Text className="font-title text-xl text-gray-700">{title}</Text>
+      ) : (
+        <View />
+      )}
+
+      {iconRight ? (
+        <TouchableOpacity activeOpacity={0.6}>
+          {iconRight === 'plus' ? (
+            <Plus size={24} color="#1A181B" weight="regular" />
+          ) : (
+            <PencilSimpleLine size={24} color="#1A181B" weight="regular" />
+          )}
+        </TouchableOpacity>
+      ) : (
+        <View />
+      )}
+    </View>
+  )
+}
