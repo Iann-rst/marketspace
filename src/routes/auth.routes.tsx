35d75ce1d450db88a793a90fe0add3375b9a56da
Iann Rodrigues <iannrst11@gmail.com>
2023-06-07 19:13:38 -0300

    feat: Navegação stack rotas de autenticação

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,26 @@
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from '@react-navigation/native-stack'
+import { SignIn } from '../screens/SignIn'
+import { SignUp } from '../screens/SignUp'
+
+/* Tipagem das rotas de autenticação SignIn e SignUp */
+type AuthRoutesTypes = {
+  signIn: undefined
+  signUp: undefined
+}
+
+export type AuthNavigatorRoutesProps =
+  NativeStackNavigationProp<AuthRoutesTypes>
+
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutesTypes>()
+
+export function AuthRoutes() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="signIn" component={SignIn} />
+      <Screen name="signUp" component={SignUp} />
+    </Navigator>
+  )
+}
