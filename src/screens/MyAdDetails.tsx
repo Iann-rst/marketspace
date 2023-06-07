5cf19a7553bba8f9d13abce096698cec2dd385b2
Iann Rodrigues <iannrst11@gmail.com>
2023-06-07 19:55:51 -0300

    feat: Tela de detalhes do meu anúncio

---
+++ src/screens/MyAdDetails.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export function MyAdDetails() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Detalhes do meu anúncio</Text>
+    </View>
+  )
+}
