4c2b81c037564fcce769ff3cd48abc1a22217994
Iann Rodrigues <iannrst11@gmail.com>
2023-06-07 19:56:32 -0300

    feat: Tela de preview do anúncio

---
+++ src/screens/AdPreview.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export function AdPreview() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Preview do anúncio</Text>
+    </View>
+  )
+}
