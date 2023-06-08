87827f6be60a61a80757e0d5763a3a2f89ceb558
Iann Rodrigues <iannrst11@gmail.com>
2023-06-08 19:54:49 -0300

    feat: Auth Token Storage

---
+++ src/storage/storageAuthToken.ts
@@ -0,0 +1,11 @@
+export async function storageAuthTokenSave() {
+  // salva o token e refresh-token no async-storage/secure-store
+}
+
+export async function storageAuthTokenGet() {
+  // busca o token e refresh-token do usuário no async-storage/secure-store
+}
+
+export async function storageAuthTokenRemove() {
+  // remove o token e refresh-token no async-storage/secure-store
+}
