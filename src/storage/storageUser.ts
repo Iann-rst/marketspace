2707c43927eef4c9f9d9a73a20542775d5bc3a50
Iann Rodrigues <iannrst11@gmail.com>
2023-06-08 19:52:00 -0300

    feat: User Storage

---
+++ src/storage/storageUser.ts
@@ -0,0 +1,11 @@
+export async function storageUserSave() {
+  // salva no async-storage/secure-store
+}
+
+export async function storageUserGet() {
+  // busca os dados do usuário no async-storage/secure-store
+}
+
+export async function storageUserRemove() {
+  // remove os dados do usuário no async-storage/secure-store
+}
