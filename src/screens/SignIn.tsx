3fb073d4e0139ce0ce460ee31cb00f08a7cdcad8
Iann Rodrigues <iannrst11@gmail.com>
2023-06-09 14:14:53 -0300

    feat: Validação com React-hook-form

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,17 +1,53 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import { useCallback, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { AuthNavigatorRoutesProps } from '../routes/auth.routes'
 
-import { Eye, EyeClosed } from 'phosphor-react-native'
-import { useState } from 'react'
 import { Button } from '../components/Button'
 import { Input } from '../components/Input'
+
+import { Eye, EyeClosed } from 'phosphor-react-native'
 import Logo from './../assets/logo.svg'
+
+import {
+  FormDataSignInProps,
+  signInSchemaValidation,
+} from '../utils/schema/SignIn'
 
 export function SignIn() {
   const [passwordVisible, setPasswordVisible] = useState(false)
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const { navigate } = useNavigation<AuthNavigatorRoutesProps>()
 
-  async function handleSignIn() {}
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormDataSignInProps>({
+    mode: 'onSubmit',
+    resolver: zodResolver(signInSchemaValidation),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  async function handleSignIn({ email, password }: FormDataSignInProps) {
+    console.log('E-mail: ', email)
+    console.log('password: ', password)
+  }
+
+  function goSignUpScreen() {
+    navigate('signUp')
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      reset()
+    }, [reset]),
+  )
 
   return (
     <ScrollView
@@ -29,26 +65,52 @@ export function SignIn() {
           </Text>
         </View>
 
-        <View className="mt-16 w-full items-center space-y-4">
-          <Text className="font-body text-sm leading-[18.2px] text-gray-600">
+        <View className="mt-16 w-full items-center">
+          <Text className="mb-4 font-body text-sm leading-[18.2px] text-gray-600">
             Acesse sua conta
           </Text>
 
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            onChangeText={setEmail}
-            value={email}
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                className={`${
+                  errors.email?.message
+                    ? 'border border-red-400 focus:border-red-400'
+                    : ''
+                }`}
+                placeholder="E-mail"
+                keyboardType="email-address"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
           />
 
           {/* Input de senha */}
-          <View className="w-full flex-row items-center rounded-md bg-gray-100 px-3 py-4 focus:border focus:border-gray-500">
-            <Input
-              placeholder="Senha"
-              className="flex-1 px-0 py-0 focus:border-0"
-              secureTextEntry={!passwordVisible}
-              onChangeText={setPassword}
-              value={password}
+          <View
+            className={`mt-4 w-full flex-row items-center rounded-md bg-gray-100 px-3 py-4 focus:border focus:border-gray-500 ${
+              errors.password?.message
+                ? 'border border-red-400 focus:border focus:border-red-400 '
+                : ''
+            }`}
+          >
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha"
+                  className="flex-1 px-0 py-0 focus:border-0"
+                  secureTextEntry={!passwordVisible}
+                  onChangeText={onChange}
+                  value={value}
+                  onSubmitEditing={handleSubmit(handleSignIn)}
+                  returnKeyType="send"
+                />
+              )}
             />
 
             <TouchableOpacity
@@ -58,14 +120,29 @@ export function SignIn() {
               {!passwordVisible ? <EyeClosed size={20} /> : <Eye size={20} />}
             </TouchableOpacity>
           </View>
+          {/* mensagem de error */}
+          {errors.password?.message && (
+            <Text className="self-start font-title text-xs text-red-400">
+              {errors.password?.message}
+            </Text>
+          )}
         </View>
 
-        <Button className="mt-8 w-full" title="Entrar" onPress={handleSignIn} />
+        <Button
+          className="mt-8 w-full"
+          title="Entrar"
+          onPress={handleSubmit(handleSignIn)}
+        />
       </View>
 
       <View className="items-center gap-4 px-12 py-14">
         <Text>Ainda não tem acesso?</Text>
-        <Button className="w-full" title="Criar uma conta" variant="light" />
+        <Button
+          className="w-full"
+          title="Criar uma conta"
+          variant="light"
+          onPress={goSignUpScreen}
+        />
       </View>
     </ScrollView>
   )
