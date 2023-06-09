0c5dea30c6888aecbb0e22bff0c221bb9306d630
Iann Rodrigues <iannrst11@gmail.com>
2023-06-09 14:14:04 -0300

    feat: Mensagem de erro do input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,24 @@
-import { TextInput, TextInputProps } from 'react-native'
+import { Text, TextInput, TextInputProps } from 'react-native'
 
-type InputProps = TextInputProps & {}
+type InputProps = TextInputProps & {
+  errorMessage?: string
+}
 
-export function Input({ ...rest }: InputProps) {
+export function Input({ errorMessage, ...rest }: InputProps) {
   return (
-    <TextInput
-      placeholderTextColor="#9F9BA1"
-      className="w-full rounded-md bg-gray-100 px-3 py-4 font-body text-base leading-[130%] text-gray-600 focus:border focus:border-gray-500"
-      autoCapitalize="none"
-      autoCorrect={false}
-      {...rest}
-    />
+    <>
+      <TextInput
+        placeholderTextColor="#9F9BA1"
+        className="w-full rounded-md bg-gray-100 px-3 py-4 font-body text-base leading-[130%] text-gray-600 focus:border focus:border-gray-500"
+        autoCapitalize="none"
+        autoCorrect={false}
+        {...rest}
+      />
+      {errorMessage && (
+        <Text className="self-start font-title text-xs text-red-400">
+          {errorMessage}
+        </Text>
+      )}
+    </>
   )
 }
