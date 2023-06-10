989054e99c8b4dcdca8d6c950d6e8887374611a9
Iann Rodrigues <iannrst11@gmail.com>
2023-06-10 18:55:47 -0300

    feat: Métodos de salvar no storage o User e Tokens

--- src/storage/storageAuthToken.ts
+++ src/storage/storageAuthToken.ts
@@ -1,11 +1,31 @@
-export async function storageAuthTokenSave() {
-  // salva o token e refresh-token no async-storage/secure-store
+import * as SecureStore from 'expo-secure-store'
+import { AUTH_TOKEN_STORAGE } from './storageConfig'
+
+type StorageAuthTokenProps = {
+  token: string
+  refresh_token: string
+}
+
+export async function storageAuthTokenSave({
+  token,
+  refresh_token,
+}: StorageAuthTokenProps) {
+  await SecureStore.setItemAsync(
+    AUTH_TOKEN_STORAGE,
+    JSON.stringify({ token, refresh_token }),
+  )
 }
 
 export async function storageAuthTokenGet() {
-  // busca o token e refresh-token do usuário no async-storage/secure-store
+  const storage = await SecureStore.getItemAsync(AUTH_TOKEN_STORAGE)
+
+  const { token, refresh_token }: StorageAuthTokenProps = storage
+    ? JSON.parse(storage)
+    : {}
+
+  return { token, refresh_token }
 }
 
 export async function storageAuthTokenRemove() {
-  // remove o token e refresh-token no async-storage/secure-store
+  await SecureStore.deleteItemAsync(AUTH_TOKEN_STORAGE)
 }
--- src/storage/storageUser.ts
+++ src/storage/storageUser.ts
@@ -1,11 +1,19 @@
-export async function storageUserSave() {
-  // salva no async-storage/secure-store
+import * as SecureStore from 'expo-secure-store'
+import { UserDTO } from '../dtos/UserDTO'
+import { USER_STORAGE } from './storageConfig'
+
+export async function storageUserSave(user: UserDTO) {
+  await SecureStore.setItemAsync(USER_STORAGE, JSON.stringify(user))
 }
 
 export async function storageUserGet() {
-  // busca os dados do usuário no async-storage/secure-store
+  const storage = await SecureStore.getItemAsync(USER_STORAGE)
+
+  const user: UserDTO = storage ? JSON.parse(storage) : []
+
+  return user
 }
 
 export async function storageUserRemove() {
-  // remove os dados do usuário no async-storage/secure-store
+  await SecureStore.deleteItemAsync(USER_STORAGE)
 }
