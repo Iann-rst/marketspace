ccbf00bc6ec011247e71a8819e913ab60fb04e1b
Iann Rodrigues <iannrst11@gmail.com>
2023-06-15 21:35:37 -0300

    feat: name e foto do usuário

--- src/components/InfoProfile.tsx
+++ src/components/InfoProfile.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from 'react-native'
+import { api } from '../services/api'
 import { Avatar } from './Avatar'
 
 interface InfoProfileProps {
@@ -6,18 +7,18 @@ interface InfoProfileProps {
   name: string
 }
 
-export function InfoProfile() {
+export function InfoProfile({ name, userImage }: InfoProfileProps) {
   return (
     <View className="w-full flex-row items-center space-x-2">
       <Avatar
         borderIsBlue
         borderWidth={2}
         size={30}
-        source={{ uri: 'https://github.com/Iann-rst.png' }}
+        source={{ uri: `${api.defaults.baseURL}/images/${userImage}` }}
       />
 
       <Text className="font-body text-sm leading-[130%] text-gray-700">
-        Iann Rodrigues
+        {name}
       </Text>
     </View>
   )
