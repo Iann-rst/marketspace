2cc8e3ab39a9eeaa744ea4865eefc69b8a6d46c1
Iann Rodrigues <iannrst11@gmail.com>
2023-06-15 21:34:55 -0300

    fix: value

--- src/utils/paymentMethods/getPaymentMethod.ts
+++ src/utils/paymentMethods/getPaymentMethod.ts
@@ -1,12 +1,13 @@
 type GetProps = {
   label: string
   checked: boolean
+  value: string
 }
 
 export function getPaymentMethod(data: GetProps[]) {
   const response = data.filter((payment) => payment.checked === true)
 
-  const paymentMethods = response.map((payment) => payment.label)
+  const paymentMethods = response.map((payment) => payment.value)
 
   return paymentMethods
 }
