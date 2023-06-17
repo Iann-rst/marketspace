b7ac2210d15b030828bed913ee7697637919e9db
Iann Rodrigues <iannrst11@gmail.com>
2023-06-16 22:11:43 -0300

    fix: Info do card de produto

--- src/components/AdvertCard.tsx
+++ src/components/AdvertCard.tsx
@@ -6,21 +6,16 @@ import {
   TouchableOpacityProps,
   View,
 } from 'react-native'
+import { ProductDTO } from '../dtos/ProductDTO'
+import { api } from '../services/api'
 import { Avatar } from './Avatar'
 import { Overlay } from './Overlay'
 
-type AdvertCardProps = TouchableOpacityProps & {
-  isActive?: boolean
-  isNew?: boolean
-  showUser?: boolean
+type ProductDataProps = TouchableOpacityProps & {
+  product: ProductDTO
 }
 
-export function AdvertCard({
-  isActive = true,
-  isNew = true,
-  showUser = true,
-  ...rest
-}: AdvertCardProps) {
+export function AdvertCard({ product, ...rest }: ProductDataProps) {
   const dimension = Dimensions.get('window').width - 48
   const width = dimension / 2 - 10
 
@@ -35,19 +30,23 @@ export function AdvertCard({
     >
       <View>
         <Image
-          source={{ uri: 'https://github.com/Iann-rst.png' }}
+          source={{
+            uri: `${api.defaults.baseURL}/images/${product.product_images[0].path}`,
+          }}
           alt=""
           className="h-24 w-full rounded-lg"
           resizeMode="cover"
         />
 
         <View className="absolute left-0 top-0 w-full flex-row items-start justify-between p-1">
-          {showUser ? (
+          {product.user ? (
             <Avatar
               size={24}
               borderIsBlue={false}
               borderWidth={1}
-              source={{ uri: 'https://github.com/Iann-rst.png' }}
+              source={{
+                uri: `${api.defaults.baseURL}/images/${product.user.avatar}`,
+              }}
             />
           ) : (
             <View />
@@ -55,35 +54,40 @@ export function AdvertCard({
 
           <View
             className={`items-center justify-center rounded-full ${
-              isNew ? 'bg-blue-700' : 'bg-gray-600'
+              product.is_new ? 'bg-blue-700' : 'bg-gray-600'
             } px-2 py-0.5`}
           >
             <Text className="font-title text-xs uppercase leading-[130%] text-gray-100">
-              {isNew ? 'novo' : 'usado'}
+              {product.is_new ? 'novo' : 'usado'}
             </Text>
           </View>
         </View>
 
         {/* Overlay */}
-        {!isActive && <Overlay />}
+        {!product.is_active && <Overlay />}
       </View>
 
       {/* Details */}
       <View>
         <Text
           className={`font-body text-sm ${
-            isActive ? 'text-gray-600' : 'text-gray-400'
+            product.is_active ? 'text-gray-600' : 'text-gray-400'
           }`}
         >
-          Tênis vermelho
+          {product.name}
         </Text>
         <Text
           className={`text-xs ${
-            isActive ? 'font-title text-gray-700' : 'font-body text-gray-400'
+            product.is_active
+              ? 'font-title text-gray-700'
+              : 'font-body text-gray-400'
           }`}
         >
           R$
-          <Text className="text-base"> 59,90</Text>
+          <Text className="text-base">
+            {' '}
+            {parseFloat(product.price).toFixed(2)}
+          </Text>
         </Text>
       </View>
     </TouchableOpacity>
