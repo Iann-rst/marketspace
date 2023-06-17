678030597b6275ee42a3b784304023535d74e5cf
Iann Rodrigues <iannrst11@gmail.com>
2023-06-16 22:12:13 -0300

    feat: Componente Overlay

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,8 +1,16 @@
 import { Text, View } from 'react-native'
 
-export function Overlay() {
+type OverlayProps = {
+  isCentralized?: boolean
+}
+
+export function Overlay({ isCentralized = false }: OverlayProps) {
   return (
-    <View className="absolute h-full w-full justify-end rounded-lg bg-gray-700/60 p-2">
+    <View
+      className={`absolute h-full w-full ${
+        isCentralized ? 'items-center justify-center' : 'justify-end rounded-lg'
+      } bg-gray-700/60 p-2`}
+    >
       <Text className="font-title text-xs uppercase text-gray-100">
         anúncio desativado
       </Text>
