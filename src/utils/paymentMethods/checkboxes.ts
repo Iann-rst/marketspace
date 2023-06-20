d8903a0985a30755d538865ae3b63761edce9736
Iann Rodrigues <iannrst11@gmail.com>
2023-06-19 21:17:02 -0300

    feat: Estado inicial dos CheckBoxes

---
+++ src/utils/paymentMethods/checkboxes.ts
@@ -0,0 +1,27 @@
+export const CheckBoxes = [
+  {
+    label: 'Boleto',
+    checked: false,
+    value: 'boleto',
+  },
+  {
+    label: 'Pix',
+    checked: false,
+    value: 'pix',
+  },
+  {
+    label: 'Dinheiro',
+    checked: false,
+    value: 'cash',
+  },
+  {
+    label: 'Cartão de Crédito',
+    checked: false,
+    value: 'card',
+  },
+  {
+    label: 'Depósito Bancário',
+    checked: false,
+    value: 'deposit',
+  },
+]
