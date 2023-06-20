d2e8cb0112fc6339d26a9b3a4d378e3663f623ec
Iann Rodrigues <iannrst11@gmail.com>
2023-06-19 21:32:13 -0300

    feat: Abrir Modal e buscar anúncio

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,8 +1,13 @@
 import { MagnifyingGlass, Sliders } from 'phosphor-react-native'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, TextInputProps, TouchableOpacity, View } from 'react-native'
 import { Input } from './Input'
 
-export function SearchInput() {
+type SearchInputProps = TextInputProps & {
+  search: () => void
+  openModal: () => void
+}
+
+export function SearchInput({ search, openModal, ...rest }: SearchInputProps) {
   return (
     <View className="space-y-3">
       <Text className="font-body text-sm leading-[18.2px] text-gray-500">
@@ -10,15 +15,12 @@ export function SearchInput() {
       </Text>
 
       <View className="flex-row items-center space-x-3 rounded-md bg-gray-100 px-3 py-4 focus:border focus:border-gray-500">
-        <Input
-          className="flex-1 p-0 focus:border-0"
-          placeholder="Buscar anúncio"
-        />
-        <TouchableOpacity>
+        <Input className="flex-1 p-0 focus:border-0" {...rest} />
+        <TouchableOpacity onPress={search}>
           <MagnifyingGlass size={20} weight="bold" color="#3E3A40" />
         </TouchableOpacity>
         <View className="h-5 w-0 border-l border-gray-400/50" />
-        <TouchableOpacity>
+        <TouchableOpacity onPress={openModal}>
           <Sliders size={20} weight="bold" color="#3E3A40" />
         </TouchableOpacity>
       </View>
