a42207531c474e590ed97dc9fd24bcc698dbb52c
Iann Rodrigues <iannrst11@gmail.com>
2023-06-23 17:11:25 -0300

    fix: Loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,16 @@
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 
-import { View } from 'react-native'
-import { Loading } from '../components/Loading'
 import { useAuth } from '../hooks/useAuth'
 import { theme } from '../theme'
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
-  const { user, isLoadingUser } = useAuth()
+  const { user } = useAuth()
 
   const themeNavigation = DefaultTheme
   themeNavigation.colors.background = theme.colors.gray[200]
 
-  if (isLoadingUser) {
-    return (
-      <View className="flex-1 items-center justify-center bg-gray-200">
-        <Loading />
-      </View>
-    )
-  }
   return (
     <NavigationContainer theme={themeNavigation}>
       {user.id ? <AppRoutes /> : <AuthRoutes />}
